a702a0affd768b94306c827fb0a3aeebb73d413b
Rhushikesh <rhushikeshnandodkar2003@gmail.com>
2026-03-18 10:36:58 +0530

    debaug true

--- project/production.py
+++ project/production.py
@@ -1,7 +1,7 @@
 from .settings import *  # noqa: F401,F403
 import dj_database_url
 
-DEBUG = False
+DEBUG = True
 
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
